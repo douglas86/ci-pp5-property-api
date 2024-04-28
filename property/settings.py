0d9ef893a33d86f139425748d86017b241374c7d
douglas <douglasmaxton@gmail.com>
2024-04-28 19:08:11 +0100

    added: browser reload only for when in development

--- property/settings.py
+++ property/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SETTINGS_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
+LOCALHOST = os.environ.get('DJANGO_LOCALHOST')
 
 ALLOWED_HOSTS = ["*"]
 
@@ -75,12 +76,15 @@ ASGI_APPLICATION = 'property.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if LOCALHOST == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+    INSTALLED_APPS.append('django_browser_reload')
+    MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- property/urls.py
+++ property/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for property project.
+URL configuration for a property project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .settings import LOCALHOST
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if LOCALHOST == 'True':
+    urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")))
