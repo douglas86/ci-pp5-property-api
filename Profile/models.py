461e356992f2096192aa2536f9cc77ffe1b40e90
douglas <douglasmaxton@gmail.com>
2024-04-30 15:30:45 +0100

    added: docstring for Profile model

--- Profile/models.py
+++ Profile/models.py
@@ -4,6 +4,10 @@ from rest_framework.authtoken.admin import User
 
 # Create your models here.
 class Profile(models.Model):
+    """
+    Profile model is used to store user profile information
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
