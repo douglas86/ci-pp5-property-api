76e966776ffb09327229d809687175a0814c762e
douglas <douglasmaxton@gmail.com>
2024-05-02 12:57:59 +0100

    added: get method for Profile view fetching uses information

--- Profile/views.py
+++ Profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from adrf.viewsets import ViewSet
-from django.views.generic import TemplateView
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.admin import User
 
@@ -8,14 +10,15 @@ from Profile.models import Profile
 
 
 # Create your views here.
-class ProfileView(TemplateView):
+class ProfileView(APIView):
     """
     This view is used for displaying profile information
     """
 
     model = Profile
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
 
-    def get_context_data(self, **kwargs):
-        context = super(ProfileView, self).get_context_data(**kwargs)
-
-        return {'context', context}
+    def get(self, request):
+        user = [user for user in self.model.objects.filter(user=request.user)]
+        return Response(user)
