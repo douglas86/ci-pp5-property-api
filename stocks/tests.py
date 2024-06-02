c0175b93706384a8739796dc1d44a472fad06026
douglas <douglasmaxton@gmail.com>
2024-06-02 16:46:59 +0100

    created: test for admin user can create a new property

--- stocks/tests.py
+++ stocks/tests.py
@@ -2,14 +2,12 @@ import io
 
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
+from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework import status
 from rest_framework.test import APITestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
-
-from Profile.tests import TestUser
-from .models import Stocks
-import io
 from PIL import Image
+
+from .models import Stocks
 
 
 def get_temporary_image():
@@ -81,31 +79,14 @@ class TestStocks(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_logged_in_user_can_create_property(self):
+    def test_admin(self):
         """
-        Test if a user can create a Property
+        Test if you are logged in and you are the admin can create a property
         :return:
         """
+
         self.client.login(username='admin', password='IAMininGLOrN')
 
-        temp_image = get_temporary_image()
-        image_file = SimpleUploadedFile("temp_image.jpg", temp_image.read(), content_type="image/jpeg")
-
-        url = '/stocks/create/'
-
-        data = {
-            "owner": self.admin_user.id,
-            "property_image": image_file,
-            "property_address": "7 - 11 High Street, Reigate",
-            "property_area": "Reigate",
-            "area_code": "RH29AA",
-            "rent": 700,
-            "created_at": "2024-02-15T13:20:30+03:00",
-            "updated_at": "2024-02-15T13:20:30+03:00"
-        }
-
-        response = self.client.post(url, data, format='multipart')
+        response = self.client.post(self.url, data=self.posting_data(), format='multipart')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Stocks.objects.count(), 1)
-        self.assertEqual(Stocks.objects.get().owner, self.admin_user)
