5b5fb1570526116e4dfd149d0ab9f74609eb95ea
douglas <douglasmaxton@gmail.com>
2024-06-02 16:38:51 +0100

    created: test if the logged-in user is not the admin

--- stocks/tests.py
+++ stocks/tests.py
@@ -39,22 +39,15 @@ class TestStocks(APITestCase):
         # create admin and user
         self.admin_user = User.objects.create_superuser(username='admin', password='IAMininGLOrN')
         self.regular_user = User.objects.create_user(username='user', password='IAMininGLOrN')
-        self.temp_image = get_temporary_image()
         self.url = '/stocks/create/'
 
-    def image(self):
-        """
-        temporary image
-        :return:
-        """
-
-        image_file = SimpleUploadedFile('temp_image.jpg', self.temp_image.read(), content_type='image/jpeg')
-        return image_file
-
     def posting_data(self):
+        temp_image = get_temporary_image()
+        image_file = SimpleUploadedFile("temp_image.jpg", temp_image.read(), content_type="image/jpeg")
+
         data = {
             "owner": self.admin_user.id,
-            "property_image": self.image,
+            "property_image": image_file,
             "property_address": "7 - 11 High Street, Reigate",
             "property_area": "Reigate",
             "area_code": "RH29AA",
@@ -65,52 +58,54 @@ class TestStocks(APITestCase):
 
         return data
 
-    def response(self):
-        response = self.client.post(self.url, data=self.posting_data(), format='multipart')
-        return response.status_code
-
     def test_cannot_create_property(self):
         """
         Test that if you are not logged in, you cannot create a property
         :return:
         """
 
-        self.assertEqual(self.response(), status.HTTP_403_FORBIDDEN)
+        response = self.client.post(self.url, data=self.posting_data(), format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_not_admin(self):
         """
-        Test that if you are logged in and not the admin
+        Test that if you are logged in and not the admin,
         You cannot create a property
         :return:
         """
 
+        self.client.login(username='user', password='IAMininGLOrN')
+
         response = self.client.post(self.url, data=self.posting_data(), format='multipart')
 
-    # def test_logged_in_user_can_create_property(self):
-    #     """
-    #     Test if a user can create a Property
-    #     :return:
-    #     """
-    #     self.client.login(username='user', password='IAMininGLOrN')
-    #
-    #     temp_image = get_temporary_image()
-    #     image_file = SimpleUploadedFile("temp_image.jpg", temp_image.read(), content_type="image/jpeg")
-    #
-    #     url = '/stocks/create/'
-    #
-    #     data = {
-    #         "owner": self.admin_user.id,
-    #         "property_image": image_file,
-    #         "property_address": "7 - 11 High Street, Reigate",
-    #         "property_area": "Reigate",
-    #         "area_code": "RH29AA",
-    #         "rent": 700,
-    #         "created_at": "2024-02-15T13:20:30+03:00",
-    #         "updated_at": "2024-02-15T13:20:30+03:00"
-    #     }
-    #
-    #     response = self.client.post(url, data, format='multipart')
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(Stocks.objects.count(), 1)
-    #     self.assertEqual(Stocks.objects.get().owner, self.admin_user)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_in_user_can_create_property(self):
+        """
+        Test if a user can create a Property
+        :return:
+        """
+        self.client.login(username='admin', password='IAMininGLOrN')
+
+        temp_image = get_temporary_image()
+        image_file = SimpleUploadedFile("temp_image.jpg", temp_image.read(), content_type="image/jpeg")
+
+        url = '/stocks/create/'
+
+        data = {
+            "owner": self.admin_user.id,
+            "property_image": image_file,
+            "property_address": "7 - 11 High Street, Reigate",
+            "property_area": "Reigate",
+            "area_code": "RH29AA",
+            "rent": 700,
+            "created_at": "2024-02-15T13:20:30+03:00",
+            "updated_at": "2024-02-15T13:20:30+03:00"
+        }
+
+        response = self.client.post(url, data, format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Stocks.objects.count(), 1)
+        self.assertEqual(Stocks.objects.get().owner, self.admin_user)
