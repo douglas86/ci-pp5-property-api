47645ca6ecff54141b7c3292c0c4f12758826f3d
douglas <douglasmaxton@gmail.com>
2024-06-06 14:14:48 +0100

    added: adrf package to an installed apps list

--- property/settings.py
+++ property/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'cloudinary',
+    'adrf',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
