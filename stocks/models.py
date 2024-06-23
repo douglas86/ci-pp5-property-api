4fc4e07a34abe601d340432e313b38621a8fe100
douglas <douglasmaxton@gmail.com>
2024-06-23 21:34:04 +0100

    feat: stores property data from React

--- stocks/models.py
+++ stocks/models.py
@@ -11,6 +11,7 @@ def upload_location(instance, filename):
 class Stocks(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     property_image = models.ImageField(upload_to=upload_location, default='default_profile_rkmhff')
+    # property_image = models.CharField(max_length=1000)
     property_address = models.CharField(max_length=100)
     property_area = models.CharField(max_length=100)
     area_code = models.CharField(max_length=100)
--- stocks/views.py
+++ stocks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import permissions, generics, status
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView, Response
-from django.contrib.auth.mixins import LoginRequiredMixin
+from adrf.viewsets import ViewSet
 
 from .models import Stocks
 from .serilizers import StockSerializer
@@ -9,16 +11,35 @@ from .serilizers import StockSerializer
 
 # Create your views here.
 # Create
-class StocksCreateView(generics.ListCreateAPIView):
+class PropertyCreateView(ViewSet):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     serializer_class = StockSerializer
-    queryset = Stocks.objects.all()
-    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
 
-    def perform_create(self, serializer):
+    message = 'You have successfully created a property.'
+    error_message = 'Something went wrong.'
+
+    def retrieve(self, request):
+        serializer = self.serializer_class(data=request.data)
+
         if serializer.is_valid():
-            serializer.save(owner=self.request.user)
-            return Response("You have successfully saved a new property", status=status.HTTP_201_CREATED)
-        return Response("There was an error saving to database", status=status.HTTP_400_BAD_REQUEST)
+            serializer.save(owner=request.user)
+            return Response({'message': self.message, 'status': status.HTTP_200_OK, 'data': serializer.data})
+        else:
+            return Response({'message': self.error_message, 'status': status.HTTP_400_BAD_REQUEST,
+                             'errors': serializer.errors})
+
+
+# class StocksCreateView(generics.ListCreateAPIView):
+#     serializer_class = StockSerializer
+#     queryset = Stocks.objects.all()
+#     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+#
+#     def perform_create(self, serializer):
+#         if serializer.is_valid():
+#             serializer.save(owner=self.request.user)
+#             return Response("You have successfully saved a new property", status=status.HTTP_201_CREATED)
+#         return Response("There was an error saving to database", status=status.HTTP_400_BAD_REQUEST)
 
 
 # Read
