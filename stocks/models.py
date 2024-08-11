45861aed2d9c93518d98141bcda5f589ebdbe836
douglas <douglasmaxton@gmail.com>
2024-08-11 22:24:07 +0100

    feat: deleted image from serializers

--- stocks/models.py
+++ stocks/models.py
@@ -10,10 +10,10 @@ def upload_location(_, filename):
 
 class Stocks(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    property_image = models.ImageField(
-        upload_to=upload_location,
-        default='https://res.cloudinary.com/dtm8tjo1f/image/upload/v1712434760/default_post_r8m7an.jpg'
-    )
+    # property_image = models.ImageField(
+    #     upload_to=upload_location,
+    #     default='https://res.cloudinary.com/dtm8tjo1f/image/upload/v1712434760/default_post_r8m7an.jpg'
+    # )
     property_address = models.CharField(max_length=100)
     property_area = models.CharField(max_length=100)
     area_code = models.CharField(max_length=100)
